f66502c34cd63de474fe55a736c649114e0268b9
John Duck <jduck1133@gmail.com>
2019-03-09 11:51:16 -0600

    Front end and User Auth Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import Header from './components/Header/Header';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Feed from './components/Feed'
+import Profile from './components/Profile'
+import ArticleView from './components/ArticleView'
+import Editor from './components/Editor'
+import requireAuthentication from './utils/requireAuth'
+import SignInWith from './components/SignInWith'
 
 class App extends Component {
-  
   render() {
+    const pathname = window.location.pathname
     return (
       <div className="App">
-        <Header/>
-        <p className="App-intro"></p>
+        { !pathname.includes('editor') ? <Header /> : '' }
+        <SignInWith />
+          <Switch>
+            <Route exact path="/" component={Feed} />
+            <Route path="/profile/:id" component={Profile} />
+            <Route path="/articleview/:id" component={ArticleView} />
+            <Route path="/editor" component={requireAuthentication(Editor)} />
+            <Route path="**" component={Feed} />
+          </Switch>
       </div>
     );
   }
